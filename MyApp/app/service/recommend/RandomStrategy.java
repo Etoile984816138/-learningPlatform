03567d3a4b012d3091811c5e2bcdcaeff64e404d
Your Name <you@example.com>
2017-06-29 14:35:49 +0800

    d

--- MyApp/app/service/recommend/RandomStrategy.java
+++ MyApp/app/service/recommend/RandomStrategy.java
@@ -1,16 +1,21 @@
 package service.recommend;
 
 import models.Course;
+import models.Study;
+import play.db.jpa.JPA;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by jsh3 on 17-6-29.
  */
 public class RandomStrategy {
 
+
+
     public List<Course> randomRecommend(int limit) {
-        return null;
+
     }
 
 
